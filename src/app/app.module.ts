0c2ee5219427d3f3d06950ff8a45410a3da24e38
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-12-15 14:27:11 +0100

    pokemon-item et pokemons-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PokemonItemComponent } from './components/pokemon-item/pokemon-item.component';
+import { PokemonsListComponent } from './components/pokemons-list/pokemons-list.component';
 @NgModule({
   declarations: [
     AppComponent,
+    PokemonItemComponent,
+    PokemonsListComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pokemons-list/pokemons-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemons-list',
+  templateUrl: './pokemons-list.component.html',
+  styleUrls: ['./pokemons-list.component.scss']
+})
+export class PokemonsListComponent {
+
+}
